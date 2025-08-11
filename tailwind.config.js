dea2994060b45b7873a9885720da90a273ca2d93
pranavjandialfit-cloud <pranavjandialfit@gmail.com>
2025-08-11 06:29:39 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: { extend: {} },
   plugins: [],
-  darkMode: 'class',
+  
 }
